cb6c9e97ba2ee156df56814b7f0deead10286cd5
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-11-30 23:47:25 +0800

    Update product detail view to use dynamic image source from storage

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    
     <div class="container mx-auto px-4 py-6">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
             <!-- Gambar Produk -->
             <div class="border rounded-lg overflow-hidden">
-                <img src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
+                <img src="{{ asset('storage/' . $product->image) }}"
                     alt="{{ $product->name }}" class="w-full">
             </div>
 
