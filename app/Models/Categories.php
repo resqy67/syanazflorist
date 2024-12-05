41e7407a61abf68810a56e772f36cc185f7fb963
Resqy Hedy Kurniawan <62694501+resqy67@users.noreply.github.com>
2024-12-05 17:44:18 +0800

    Update and rename categories.php to Categories.php

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use app\Models\products;
+use app\Models\Products;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class categories extends Model
 {
-    protected $table = 'categories';
+    protected $table = 'Categories';
     protected $fillable = ['name', 'slug'];
     public $timestamps = false;
     use HasFactory;
